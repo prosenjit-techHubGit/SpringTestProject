1ac5d95584dee90955420c4445c94bc4b493638c
Administrator <Administrator@win2012base>
2018-10-29 02:55:29 -0700

    add bean configuration for EmployeeService

--- employee-service/src/test/java/com/example/demo/test/EmployeeServiceIntegrationTest.java
+++ employee-service/src/test/java/com/example/demo/test/EmployeeServiceIntegrationTest.java
@@ -8,7 +8,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.demo.domain.Employee;
@@ -18,6 +20,15 @@ import com.example.demo.service.EmployeeService;
 @RunWith(SpringRunner.class)
 public class EmployeeServiceIntegrationTest {
 
+	 @TestConfiguration
+	    static class EmployeeServiceImplTestContextConfiguration {
+	  
+	        @Bean
+	        public EmployeeService employeeService() {
+	            return new EmployeeService();
+	        }
+	    }
+	
 	@Autowired
 	EmployeeService employeeService;
 	@MockBean
